9569a8e9dc3f9a3ed7a9a6fcbb37c8fc6e45c081
static-fuji <jyun1228haru@icloud.com>
2024-04-12 16:58:39 +0900

    [add] 第一週課題1-1

--- 01/src/exercise1/CubicRoot.java
+++ 01/src/exercise1/CubicRoot.java
@@ -1,9 +1,24 @@
-package template;
+package exercise1;
 
 /**
- * @author YOUR NAME bxxxxxxx
+ * @author g2124040 藤本陽人
  */
-public class Template {
+public class CubicRoot {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
+        System.out.println(calcCBRoot(80));
+    }
+
+    public static double calcCBRoot(int m) {
+        double a=0;
+        double b=1;
+
+        for (int i = 0;i < 10; i++){
+            a = (2*b+(m/(b*b)))/3.0;
+            b = a;
+        }
+        return a;
     }
 }
