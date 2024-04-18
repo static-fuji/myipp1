64fcc28cb1d1287adac7be20f6989acebbcc8669
static-fuji <jyun1228haru@icloud.com>
2024-04-18 15:00:35 +0900

    [update] Docker image の変更

--- 01/src/exercise1/CubicRoot.java
+++ 01/src/exercise1/CubicRoot.java
@@ -8,16 +8,25 @@ public class CubicRoot {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(calcCBRoot(80));
+        System.out.println(calcCBRoot(0));
     }
 
     public static double calcCBRoot(int m) {
         double a=0;
         double b=1;
+        double epsilon = 1e-10;
 
-        for (int i = 0;i < 10; i++){
-            a = (2*b+(m/(b*b)))/3.0;
-            b = a;
+        if (m==0||m<0){
+            a=0;
+        }else{
+            while (true){
+                a = (2*b+(m/(b*b)))/3;
+                if (Math.abs(a-b) >= epsilon){
+                    b = a;
+                }else{
+                    break;
+                }          
+            }
         }
         return a;
     }
--- ipp1-build.sh
+++ ipp1-build.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
 if [ $# -lt 3 ]; then
-    echo "Usage: $0 <jar_file> <main_class> <path_to_src>"
+    echo "Usage: $0 <jar_file> <main_class> <path_to_src> [out_path] [src_copy_target1] [src_copy_target2] ..."
     exit 1
 fi
 
-DOCKER_IMG=openjdk
+DOCKER_IMG=eclipse-temurin:17
 JAR=$1
 MAIN=$2
 SRC_PATH=$3
+OUT_VOL=""
+OUT_PATH=/app
+if [ $# -gt 3 ]; then
+    OUT_VOL="-v $(realpath $4):/out"
+    OUT_PATH=/out
+fi
+CP_TARGETS="*"
+if [ $# -gt 4 ]; then
+    CP_TARGETS=${@:5}
+fi
 
 docker run --rm -it \
+    ${OUT_VOL} \
     -v $PWD:/app \
     -v $(realpath ${SRC_PATH}):/src \
     -w /work \
     ${DOCKER_IMG} \
-    sh -c "cp -r /src/* /work/ &&
+    sh -c "cd /src && 
+    cp -r --parents ${CP_TARGETS} /work/ &&
+    cd /work &&
     javac **/*.java &&
-    jar cvf /app/${JAR} ./ &&
+    jar cvf ${OUT_PATH}/${JAR} ./ &&
     echo Main-Class: ${MAIN} > main.mf &&
-    jar uvfm /app/${JAR} main.mf || /bin/bash"
+    jar uvfm ${OUT_PATH}/${JAR} main.mf || /bin/bash"
